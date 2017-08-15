995a4f7b7c19be035e6ac213cedca7a40a5100f5
Thorsten Bux <thorsten.bux_c@daqri.com>
2017-08-15 14:35:12 +1200

    sanity

--- src/app/resume.service.ts
+++ src/app/resume.service.ts
@@ -1,21 +1,19 @@
-import { Injectable } from '@angular/core';
-import { resume_data } from './mock-resume';
+import { Injectable } from "@angular/core";
+import { resume_data } from "./mock-resume";
 import { Resume } from "./classes/resume";
 
 @Injectable()
 export class ResumeService {
 
-    constructor() { }
-
-    getResumeData(): Promise<Resume>{
+    getResumeData(): Promise<Resume> {
         return Promise.resolve(resume_data);
     }
 
-    getTopLevelResumeData(from:number = 0, to:number = undefined):Promise<string[]>{
-        return this.getResumeData().then(resume_data => Object.keys(resume_data).slice(from,to));
+    getTopLevelResumeData(from: number = 0, to: number = undefined): Promise<string[]> {
+        return this.getResumeData().then(resume_data => Object.keys(resume_data).slice(from, to));
     }
 
-    getResumeDetail(identifier:string){
+    getResumeDetail(identifier: string) {
         console.log("ResumeService identifier: " + identifier);
         return this.getResumeData().then(resume_data => resume_data[identifier]);
     }
