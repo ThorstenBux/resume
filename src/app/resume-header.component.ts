6b015bf4c4afa78070c77e8871ed1e72bc2a0cc3
Thorsten Bux <thorsten.bux_c@daqri.com>
2017-08-17 14:33:17 +1200

    spinner

--- src/app/resume-header.component.ts
+++ src/app/resume-header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Resume } from "./classes/resume";
 import { ResumeService } from "./resume.service";
+import { ANIMATION_TYPES } from "ngx-loading";
+
 
 @Component({
     selector: "app-resume-header",
@@ -9,8 +11,11 @@ import { ResumeService } from "./resume.service";
 
 export class ResumeHeaderComponent implements OnInit {
     resumeDataBasics: {};
+    ANIMATION_TYPES: {};
 
-    constructor(private resumeService: ResumeService) { }
+    constructor(private resumeService: ResumeService) {
+        this.ANIMATION_TYPES = ANIMATION_TYPES;
+     }
 
     ngOnInit() {
         this.getResumeHeaderData();
